5b06e90a4c16297d7c7520d7ec648b941e152ee1
Tofik Hasanov <annexcies@gmail.com>
2021-07-19 16:24:06 +0400

    fix

--- src/data/index.js
+++
@@ -1,7 +0,0 @@
-import laptops from "./laptops.json";
-import phones from "./phones.json";
-import header from "./header.json";
-
-const data = { header, laptops, phones };
-
-export default data;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./Components/App";
-import "./index.css";
+import App from "./App";
+import AppProfile from "./App-profile";
+import AppStat from "./App-stat";
 
 ReactDOM.render(
-  <>
+  <React.StrictMode>
     <App />
-  </>,
+    <AppProfile />
+    <AppStat />
+  </React.StrictMode>,
   document.getElementById("root")
 );
